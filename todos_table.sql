45e8cd57eef303692e462b91a458466270f9df04
masakinihirota <masakinihirota@gmail.com>
2023-12-26 13:08:25 +0900

    🎨 Remove unused files and code examples

---
+++ todos_table.sql
@@ -0,0 +1,11 @@
+create table
+  public.todos (
+    id uuid not null default gen_random_uuid (),
+    created_at timestamp with time zone not null default timezone ('utc'::text, now()),
+    title text null,
+    is_complete boolean null default false,
+    user_id uuid null default auth.uid (),
+    constraint todos_pkey primary key (id),
+    constraint todos_user_id_fkey foreign key (user_id) references auth.users (id)
+  ) tablespace pg_default;
+
